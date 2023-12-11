a2321a9a327bf87c774a14b8f6b29c23c23b55fe
Lily Yu <>
2023-12-11 18:49:13 -0500

    Added a prompt in NoteDisplayView for tapping instructions

--- StickAroundUI_AR/StickAroundUI_AR/NoteDisplayView.swift
+++ StickAroundUI_AR/StickAroundUI_AR/NoteDisplayView.swift
@@ -15,7 +15,15 @@ struct NoteDisplayView: View {
     var body: some View {
         ZStack {
             ARViewContainer(viewModel: viewModel).edgesIgnoringSafeArea(.all)
+                .onAppear{
+                    showingAlert = true
+                }
+                .alert(isPresented: $showingAlert, content: {
+                    Alert(title: Text("Tap on a plane to stick the note!"), dismissButton: Alert.Button.default(Text("OK")))
+                })
+            
 //            Text("TESTING TEXT")
+            
 //            if let drawing = viewModel.message.drawing {
 //                Image(uiImage: drawing).resizable().frame(width: 200, height: 200)
 //            } else {
